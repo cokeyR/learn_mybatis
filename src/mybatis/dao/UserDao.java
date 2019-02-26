53c0a260f09af9a20ef7794cfd810a4977803d61
mme <13862132540@163.com>
2019-02-27 07:52:50 +0800

    1
    Signed-off-by: mme <13862132540@163.com>

--- src/mybatis/dao/UserDao.java
+++ src/mybatis/dao/UserDao.java
@@ -1,13 +1,28 @@
 package mybatis.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
-import mybaits.po.User;
+import mybatis.po.User;
 import mybatis.tool.Factory;
 
 public class UserDao {
 	
-	public static void main(String[] args) {
+	public static void addUser() {
+		User user=new User();
+		user.setId(15);
+	
+		user.setUsername("王翔");
+		
+		SqlSession session=Factory.getFactory().openSession();
+		session.insert("user.insertUser", user);
+		//提交事务
+		session.commit();
+		session.close();
+	}
+	
+	public static void selectUser() {
 		SqlSession session = Factory.getFactory().openSession();
 		//第一个参数为（要执行的sql的 namespace+"."+id;
 		//第二个参数即为要传入的parameterType类型的参数
@@ -15,6 +30,14 @@ public class UserDao {
 		User user = session.selectOne("user.queryUserById", 1);
 		System.out.println(user);
 		
+		List<Object> users = session.selectList("user.queryUserByName", "小明");
+		
+		for(Object e:users)
+			System.out.println(e);
 		session.close();
+	}
+	
+	public static void main(String[] args) {
+		UserDao.addUser();
 	}
 }
--- src/mybatis/po/User.java
+++ src/mybatis/po/User.java
@@ -1,4 +1,4 @@
-package mybaits.po;
+package mybatis.po;
 
 import java.util.Date;
 
